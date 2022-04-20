31ce749f9a00ecc5948ccd57ffc81fd76bebf72c
brightchul <57323359+brightchul@users.noreply.github.com>
2022-04-20 17:31:22 +0900

    modify: homework2와 통신하게 수정

--- week2/homework1/src/main/java/com/week2/homework1/handler/HelloHandler.java
+++ week2/homework1/src/main/java/com/week2/homework1/handler/HelloHandler.java
@@ -2,10 +2,8 @@ package com.week2.homework1.handler;
 
 
 import com.week2.homework1.model.Hello;
-import lombok.Data;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -17,9 +15,10 @@ public class HelloHandler {
         String name = request.queryParam("name").orElse("");
         WebClient client = WebClient.create("http://localhost:8081");
 
-        return client.get().uri("/uri?name="+name).retrieve().bodyToMono(JobResponse.class).flatMap(o -> {
+        return client.get().uri("/uri?name=" + name).retrieve().bodyToMono(JobResponse.class).flatMap(o -> {
+
             Hello hello = new Hello(name, String.format("hello %s", name), o.getJob());
-           return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(Mono.just(hello), Hello.class);
+            return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(Mono.just(hello), Hello.class);
         });
     }
 }
--- week2/homework1/src/test/java/com/week2/homework1/Homework1ApplicationTests.java
+++ week2/homework1/src/test/java/com/week2/homework1/Homework1ApplicationTests.java
@@ -1,64 +1,9 @@
 package com.week2.homework1;
 
-import com.week2.homework1.model.Hello;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class Homework1ApplicationTests {
 
-    @Autowired
-    private WebTestClient webTestClient;
-
-    @Test
-    @DisplayName("/hello에 name없이 보낸다.")
-    public void testHelloNameEmpty() {
-        webTestClient.get().uri("/hello").accept(MediaType.APPLICATION_JSON).exchange().expectStatus().isOk().expectBody(Hello.class).value(hello -> {
-            Assertions.assertThat(hello.getTo()).isEqualTo("");
-            Assertions.assertThat(hello.getMessage()).isEqualTo("hello ");
-        });
-
-        webTestClient.get().uri("/hello?name=").accept(MediaType.APPLICATION_JSON).exchange().expectStatus().isOk().expectBody(Hello.class).value(hello -> {
-            Assertions.assertThat(hello.getTo()).isEqualTo("");
-            Assertions.assertThat(hello.getMessage()).isEqualTo("hello ");
-        });
-    }
-
-    @Test
-    @DisplayName("/hello에 name=james를 보낸다.")
-    public void testHelloNameJames() {
-        webTestClient.get().uri("/hello?name=james").accept(MediaType.APPLICATION_JSON).exchange().expectStatus().isOk().expectBody(Hello.class).value(hello -> {
-            Assertions.assertThat(hello.getTo()).isEqualTo("james");
-            Assertions.assertThat(hello.getMessage()).isEqualTo("hello james");
-        });
-    }
-
-    @Test
-    @DisplayName("/hello2에 name없이 보낸다.")
-    public void testHello2NameEmpty() {
-        webTestClient.get().uri("/hello2").accept(MediaType.APPLICATION_JSON).exchange().expectStatus().isOk().expectBody(Hello.class).value(hello -> {
-            Assertions.assertThat(hello.getTo()).isEqualTo("");
-            Assertions.assertThat(hello.getMessage()).isEqualTo("hello ");
-        });
-
-        webTestClient.get().uri("/hello?name=").accept(MediaType.APPLICATION_JSON).exchange().expectStatus().isOk().expectBody(Hello.class).value(hello -> {
-            Assertions.assertThat(hello.getTo()).isEqualTo("");
-            Assertions.assertThat(hello.getMessage()).isEqualTo("hello ");
-        });
-    }
-
-    @Test
-    @DisplayName("/hello2에 name=james를 보낸다.")
-    public void testHello2NameJames() {
-        webTestClient.get().uri("/hello2?name=james").accept(MediaType.APPLICATION_JSON).exchange().expectStatus().isOk().expectBody(Hello.class).value(hello -> {
-            Assertions.assertThat(hello.getTo()).isEqualTo("james");
-            Assertions.assertThat(hello.getMessage()).isEqualTo("hello james");
-        });
-    }
 
 }
