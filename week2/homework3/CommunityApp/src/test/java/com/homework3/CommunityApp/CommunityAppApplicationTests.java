f394341c438e4e2cddb238d6671b586679119f61
brightchul <57323359+brightchul@users.noreply.github.com>
2022-04-22 18:58:38 +0900

    add: 커뮤니티앱 추가

---
+++ week2/homework3/CommunityApp/src/test/java/com/homework3/CommunityApp/CommunityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homework3.CommunityApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
