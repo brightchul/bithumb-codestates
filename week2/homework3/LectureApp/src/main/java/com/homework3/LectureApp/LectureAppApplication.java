8e1dac55ec9a216b037bfb0f772370b127d410db
brightchul <57323359+brightchul@users.noreply.github.com>
2022-04-22 18:59:03 +0900

    add: 강의 앱 추가

---
+++ week2/homework3/LectureApp/src/main/java/com/homework3/LectureApp/LectureAppApplication.java
@@ -0,0 +1,13 @@
+package com.homework3.LectureApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LectureAppApplication.class, args);
+	}
+
+}
---
+++ week2/homework3/LectureApp/src/test/java/com/homework3/LectureApp/LectureAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homework3.LectureApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
